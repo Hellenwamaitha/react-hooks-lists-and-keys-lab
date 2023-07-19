18d9df5a1aa2403dc139a74aac02ff5bae2f2667
hellenwamaitha <hellenwama254@gmail.com>
2023-07-19 12:11:27 +0300

    list and key

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    <ul>
+      <li><a href="#home">home</a></li>
+      <li><a href="#about">about</a></li>
+      <li><a href="#projects">projects</a></li>
+    </ul>
+
+    
+    }
+    </nav>;
 }
 
 export default NavBar;
